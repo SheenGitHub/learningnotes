2195d541d22f0f55a6f83040abf92d1a703c7b12
zhouxuejing <Zxj123456>
2021-04-08 19:13:09 +0800

    删除git删除的文件

--- pc/src/main/java/com/sheen/pc/constant/BaseConstant.java
+++
@@ -1,9 +0,0 @@
-package com.sheen.pc.constant;
-
-/**
- * Created by zxj7044 on 2018-11-8.
- */
-public class BaseConstant {
-    public static final String UTF8 = "UTF-8";
-    public static final String MD5 = "MD5";
-}
--- pc/src/main/java/com/sheen/pc/exception/ServiceException.java
+++
@@ -1,9 +0,0 @@
-package com.sheen.pc.exception;
-
-/**
- * Created by zxj7044 on 2018-11-6.
- */
-public interface ServiceException {
-    Integer getCode();
-    String getMessage();
-}
